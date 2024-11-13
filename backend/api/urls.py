148b0a6329e0f1d2463249e497219a2549d57b73
Yordanos Kassa <jordan.kebede72@gmail.com>
2024-11-13 18:01:44 -0500

    wip: getstarted component

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     path('get_avatars/', get_avatars, name='get_avatars'),
+    #path('save_api/', views.SaveAPI
+
 ]
